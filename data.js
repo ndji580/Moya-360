a20f56e443b0d7ae92305e4781d768c70711b4a0
ndji580 <ndji.2012@gmail.com>
2026-02-08 19:29:09 +0300

    i

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "Панорама(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08558570515360309,
+        "pitch": 0.051912163165347636,
+        "fov": 1.0254404596127644
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08558570515360309,
+          "pitch": 0.051912163165347636,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.3273728419239852,
+          "pitch": 0.04091847588980002,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "Панорама(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1197725228978932,
+        "pitch": 0.022637629951535843,
+        "fov": 0.9766679021128111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1197725228978932,
+          "pitch": 0.022637629951535843,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.13442200242977975,
+          "pitch": 0.019074800865343633,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "Панорама(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.12375820364838397,
+        "pitch": -0.01483691208585114,
+        "fov": 1.0089100218356684
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.592000566611171,
+          "pitch": 0.06367469444115059,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.9208399506878902,
+          "pitch": 0.10528904566413289,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "Панорама(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5727003751212312,
+        "pitch": 0.07161126110205984,
+        "fov": 1.416600946891962
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9751358298540875,
+          "pitch": 0.20469883431002955,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.5727003751212312,
+          "pitch": 0.07161126110205984,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
